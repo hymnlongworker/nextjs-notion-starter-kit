f07541e1236c057bfb8e72f82a04cb3f706e4c08
LongPingJiang <hymnlongworker@gmail.com>
2023-11-25 23:32:17 +0800

    配置

--- site.config.ts
+++ site.config.ts
@@ -2,8 +2,8 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'b279d632909c4f4fa7cf2ad35b475cce',
-
+  // https://joyous-sphynx-82c.notion.site/my-blog-972d00bcdb2144c1b621a5e7a5ed10c9?pvs=4
+  rootNotionPageId: '972d00bcdb2144c1b621a5e7a5ed10c9',
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
